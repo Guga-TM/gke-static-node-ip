ac337684ebe3c200571f7c79ac5032c32a733dc2
George Mikulich <aragornguga@gmail.com>
2026-04-08 17:40:05 +0300

    several improvements: - add values for helm chart - improve distributor: handle unschedulable nodes, improve control loop, handle cases when IPs and nodes count do not match

--- README.md
+++ README.md
@@ -30,3 +30,16 @@ Runs on the nodes except the ones for which IP needs to be controlled. Sends req
 ## Optional ENV variables
 - `LOG_LEVEL` - one of `info`, `error`
 - `CHECK_RATE_SECONDS` - interval between requests to check current IP address on the Controller
+
+## Note about permissions
+
+This project assumes that you're using Workload Identity Federation in your GKE cluster. Please add required permissions for GCP Service Account that you specify in helm/values.yaml. Also don't forget to add IAM Policy Binding:
+
+```
+export SA_NAME="your-sa-name"
+export PROJECT_ID="your-project-id"
+gcloud iam service-accounts add-iam-policy-binding $SA_NAME@$PROJECT_ID.iam.gserviceaccount.com \
+    --role roles/iam.workloadIdentityUser \
+    --member "serviceAccount:$PROJECT_ID.svc.id.goog[gke-static-node-ip/gke-static-node-ip]" \
+    --project "$PROJECT_ID"
+```
--- distributor.py
+++ distributor.py
@@ -30,8 +30,13 @@ def get_k8s_nodes_from_nodepool(nodepool):
 
     # List nodes matching the label selector
     nodes = v1.list_node(label_selector=label_selector)
+
+    schedulable_nodes = [
+        node.metadata.name for node in nodes.items 
+        if not node.spec.unschedulable
+    ]
     
-    return nodes.items
+    return schedulable_nodes
 
 def get_zone_of_k8s_node(node):
     zone_label = 'topology.gke.io/zone'
@@ -47,23 +52,53 @@ def get_zone_of_k8s_node(node):
 def assign_ips_to_nodes(nodes, ips):
     this_nodepool_assignment_config = defaultdict(dict)
     for node in nodes:
-        node_name = node.metadata.name
-        this_nodepool_assignment_config[node_name]['desired_ip'] = ips.pop()
-        this_nodepool_assignment_config[node_name]['gcp_zone'] = get_zone_of_k8s_node(node_name)
+        this_nodepool_assignment_config[node]['desired_ip'] = ips.pop()
+        this_nodepool_assignment_config[node]['gcp_zone'] = get_zone_of_k8s_node(node_name)
     return this_nodepool_assignment_config
 
-def get_process_nodes_data_from_json():
+def get_process_raw_nodes_data_from_json():
     # get data for all nodes from json
     json_data_env = os.environ['NODES_DATA_RAW']
     nodes_data_raw = json.loads(json_data_env)
 
-    nodes_data_parsed = {}
+    nodes_data_parsed = {} # resulting dictionary
 
     for nodepool in nodes_data_raw:
-        desired_ips = nodes_data_raw[nodepool]
-        desired_ips_set = set(desired_ips)
-        nodes = get_k8s_nodes_from_nodepool(nodepool)
-        nodes_data_parsed |= assign_ips_to_nodes(nodes, desired_ips_set)
+        desired_ips = nodes_data_raw[nodepool] # get data from JSON
+        desired_ips_set = set(desired_ips) # convert to Set
+        nodes = get_k8s_nodes_from_nodepool(nodepool) # get schedulable nodes in current nodepool
+        if len(nodes) == len(desired_ips_set):
+            nodes_data_parsed |= assign_ips_to_nodes(nodes, desired_ips_set)
+        elif len(nodes) > len(desired_ips_set):
+            log_error(component, f"found {len(nodes)} nodes, but have only {len(desired_ips_set)} IPs to assign")
+            log_error(component, "this may be ok if there are node upgrades now")
+        else:
+            log_error(component, f"misconfiguration - found {len(nodes)} nodes and {len(desired_ips_set)} IPs to assign")
+            log_error(component, "check your values.yaml file. Number of nodes in the nodepool and number of IPs to assign must be equal")
+            
+    
+    return nodes_data_parsed
+
+def monitor_update_nodes_data(nodes_data_parsed):
+    nodes_loaded_into_memory = {}
+    for node in nodes_data_parsed:
+        nodes_loaded_into_memory.add(node)
+    log_info(component, "got this nodes loaded into memory now:")
+    log_info(component, nodes_loaded_into_memory)
+
+    json_data_env = os.environ['NODES_DATA_RAW']
+    nodes_data_raw = json.loads(json_data_env)
+    nodes_now = {}
+    for nodepool in nodes_data_raw:
+        nodes_now |= set(get_k8s_nodes_from_nodepool(nodepool))
+    log_info(component, "actual list of nodes from nodepools that need to be managed:")
+    log_info(component, nodes_now)
+
+    if nodes_loaded_into_memory == nodes_now:
+        log_info(component, "everything is ok, no need to redistribute IPs")
+    else:
+        log_system("requesting redistribution of IP addresses: new nodes detected")
+        nodes_data_parsed = get_process_raw_nodes_data_from_json()
     
     return nodes_data_parsed
 
@@ -91,15 +126,15 @@ def update_cm_resource(nodes_data_parsed):
     except client.exceptions.ApiException as e:
         log_error(component, "exception when patching ConfigMap controller-config")
 
-
 def distributor():
     log_system("############## INITIALIZING GKE-STATIC-NODE-IP-DISTRIBUTOR ##############")
     config.load_incluster_config()
-    nodes_data_parsed = get_process_nodes_data_from_json()
+    nodes_data_parsed = get_process_raw_nodes_data_from_json()
     try:
         log_system("############## STARTING GKE-STATIC-NODE-IP-DISTRIBUTOR ##################")
         while True:
             update_cm_resource(nodes_data_parsed)
+            nodes_data_parsed = monitor_update_nodes_data(nodes_data_parsed)
             time.sleep(60)
     except KeyboardInterrupt:
         # Graceful exit on Ctrl+C
