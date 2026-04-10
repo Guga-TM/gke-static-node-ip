5b0c53f3b4039ab075c102d8c122762b3a904916
George Mikulich <aragornguga@gmail.com>
2026-04-10 11:45:55 +0300

    fix yaml name

--- distributor/distributor.py
+++ distributor/distributor.py
@@ -191,7 +191,7 @@ def create_ds_resource_from_yaml():
 
     # Create the resource
     try:
-        utils.create_from_yaml(k8s_api, 'daemonset.yaml', namespace='gke-static-node-ip')
+        utils.create_from_yaml(k8s_api, 'controller.yaml', namespace='gke-static-node-ip')
         log_info(component, "created controller daemonset")
     except client.exceptions.ApiException as e:
         log_error(component, "failed creating controller daemonset")
